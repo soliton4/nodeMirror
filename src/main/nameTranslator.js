132d6094f3fc94c9ac6643c3404a4c44c7176f38
soliton4 <matthias.behrens@gmail.com>
2013-07-30 15:04:41 +0200

    Update nameTranslator.js
    finally works in win

--- src/main/nameTranslator.js
+++ src/main/nameTranslator.js
@@ -9,17 +9,29 @@ define([
 ){
   var nameTranslator;
   var dirnamelen = 0;
+  var isWin;
   if (has("server-modules")){
     dirnamelen = config.dir.length;
+  if (process.platform == "win32" || process.platform == "win64"){
+	  isWin = true;
+	};
   };
   
   var NameTranslator = declare("NameTranslator", [
   ], {
     reduceName: function(parName){
-      return parName.substr(dirnamelen);
+	  if (isWin){
+	    return parName.substr(dirnamelen).replace("\\", "/");;
+	  }else{
+        return parName.substr(dirnamelen);
+      };
     }
     , fileName: function(parName){
-      return config.dir + parName;
+	  if (isWin){
+        return config.dir + (parName.replace("/", "\\"));
+	  }else{
+        return config.dir + parName;
+      };
     }
   });
   nameTranslator = new NameTranslator();
